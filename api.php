8a8bd0d7b70b899aa2e212105aebae24444d0d87
yogali <250618015@qq.com>
2018-03-27 23:05:15 +0800

    新增功能
    1.添加局部配置 2.添加生成js功能

---
+++ api.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type:application/json');
+echo json_encode([
+    'nickname' => '小小',
+    'age' => 18
+]);
--- src/LiTem.php
+++ src/LiTem.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace iBye;
 /**
  * http://litem.ibye.cn
- * Class LiTem version 1.0
+ * Class LiTem version 1.2.0
  * @package IBye
  */
 
@@ -16,6 +17,7 @@ class LiTem
         'routeSeparator' => '/',
         'routeKey' => 'r',
         'replacements' => [],
+        'apiReplacements' => [],
         'allowExtList' => [
             '.html',
             '.htm',
@@ -26,22 +28,27 @@ class LiTem
     //自定义函数容器
     private $functions = [];
 
+    private $jsVars = [];
+
+    private $jsBlocks = [];
+
     public function __construct($config = [])
     {
         if (!empty($config)) {
             $this->config = array_merge($this->config, $config);
-            if (!empty($config['htmlPath'])) {
-                $this->htmlPath = $config['htmlPath'];
-                $this->htmlPath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $this->htmlPath);
-                $tempChar = substr($this->htmlPath, -1);
-                if ($tempChar != DIRECTORY_SEPARATOR) {
-                    $this->htmlPath .= DIRECTORY_SEPARATOR;
-                }
-            } else {
-                $this->htmlPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'html' . DIRECTORY_SEPARATOR;
+        }
+        if (!empty($config['htmlPath'])) {
+            $this->htmlPath = $config['htmlPath'];
+            $this->htmlPath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $this->htmlPath);
+            $tempChar = substr($this->htmlPath, -1);
+            if ($tempChar != DIRECTORY_SEPARATOR) {
+                $this->htmlPath .= DIRECTORY_SEPARATOR;
             }
+        } else {
+            $this->htmlPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'html' . DIRECTORY_SEPARATOR;
         }
     }
+
     /**
      * 激活配置项
      */
@@ -63,7 +70,8 @@ class LiTem
             $routeStr = $_GET[$this->routeKey];
             $replaceStr = str_replace($this->routeSeparator, DIRECTORY_SEPARATOR, $routeStr);
             $parentDir = dirname($routeStr);
-            $this->findConfigFile($this->htmlPath . $parentDir);
+            $basename = basename($routeStr);
+            $this->findConfigFile($this->htmlPath . $parentDir, $basename);
             $this->activeConfig();
             $filePath = $this->htmlPath . $replaceStr;
             $this->dispatch($filePath);
@@ -74,7 +82,7 @@ class LiTem
      * 查找配置文件
      * @param null $dir
      */
-    private function findConfigFile($dir = null)
+    private function findConfigFile($dir = null, $pagename)
     {
         $filePath = $dir . DIRECTORY_SEPARATOR . 'litem.json';
         if (file_exists($filePath)) {
@@ -83,6 +91,11 @@ class LiTem
             fclose($file);
             $jsonArray = json_decode($fileContent, true);
             if (!empty($jsonArray)) {
+                if (!empty($jsonArray['local'][$pagename])) {
+                    $localConfig = $jsonArray['local'][$pagename];
+                    unset($jsonArray['local']);
+                    $jsonArray = array_merge_recursive($jsonArray, $this->transformConfig($localConfig));
+                }
                 $this->config = array_merge_recursive($this->config, $this->transformConfig($jsonArray));
             }
         }
@@ -112,13 +125,14 @@ class LiTem
      */
     private function render($page)
     {
-        if(!empty($_GET['options'])){
-            $options = $this->dealOptions($_GET['options']);
+        if (!empty($_GET['options'])) {
+            $options = $this->handleOptions($_GET['options']);
             $this->config['replacements']['options'] = $options;
         }
-
+        $this->handleApiReplacements();
         $page = $this->replaceReplacements($page);
         $page = $this->handleFunction($page);
+        $page = $this->handleJs($page);
 
         return $page;
     }
@@ -130,7 +144,9 @@ class LiTem
      */
     private function replaceReplacements($page)
     {
-        $keyValueList = $this->createKeyValueList('', $this->replacements);
+        $keyValueList = $this->createKeyValueList('', $this->replacements, function ($value) {
+            return is_array($value);
+        });
         $keyList = [];
         $valueList = [];
 
@@ -142,19 +158,79 @@ class LiTem
         return str_replace($keyList, $valueList, $page);
     }
 
-    private function createKeyValueList($parentKey, $array)
+    private function handleApiReplacements()
+    {
+        $keyValueList = $this->createKeyValueList('', $this->apiReplacements, function ($value) {
+            return is_array($value) && !isset($value['url']);
+        }, true);
+
+        $mh = curl_multi_init();
+        $chs = [];
+
+        $jsBlock = '';
+        foreach ($keyValueList as $key => $value) {
+            if (isset($value['type']) && $value['type'] == 'js') {
+                //js调用
+                $key = str_replace('.', '_', $key);
+                $this->addJsVar($key);
+                $jsBlock .= "
+                axios.get('$value[url]').then(function (response) {
+                    if(response.data){
+                        _$$key = response.data;
+                    }
+                })
+                .catch(function (error) {
+                console.log(error);
+                });
+                ";
+            } else {
+                //php调用
+                $ch = curl_init($value['url']);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_multi_add_handle($mh, $ch);
+                $chs[$key] = $ch;
+            }
+        }
+
+        if(!empty($jsBlock)){
+            $this->addJsBlock($jsBlock);
+        }
+
+        if(!empty($chs)){
+            $running = 0;
+            do {
+                curl_multi_exec($mh, $running);
+                curl_multi_select($mh);
+            } while ($running > 0);
+
+            foreach ($chs as $key => $ch) {
+                $val = (string)curl_multi_getcontent($ch);
+                $this->addReplacement($key, $val);
+                curl_multi_remove_handle($mh, $ch);
+            }
+        }
+    }
+
+    private function createKeyValueList($parentKey, $array, $condition, $isOriginKey = false)
     {
         $keyValueList = [];
 
         foreach ($array as $key => $value) {
-            if (is_array($value)) {
-                $keyValueList = array_merge($keyValueList, $this->createKeyValueList($this->createRelationKey($parentKey, $key), $value));
+            if ($condition($value)) {
+                $keyValueList = array_merge($keyValueList, $this->createKeyValueList($this->createRelationKey($parentKey, $key), $value, $condition, $isOriginKey));
                 continue;
             }
-            $keyValueList['{$' . $this->createRelationKey($parentKey, $key) . '}'] = $value;
+            $keyValueList[$this->createReplaceKey($this->createRelationKey($parentKey, $key), $isOriginKey)] = $value;
         }
 
         return $keyValueList;
+    }
+
+    private function createReplaceKey($val, $doNothing = false)
+    {
+        if ($doNothing) return $val;
+        return '{$' . $val . '}';
     }
 
     private function handleFunction($page)
@@ -173,7 +249,58 @@ class LiTem
         return $page;
     }
 
-    private function dealOptions($options)
+    private function handleJs($page){
+        if(!empty($this->jsVars)){
+            $nullJsVars = '';
+            $notNullJsVars = '';
+            foreach($this->jsVars as $key => $value){
+                if($value === null){
+                    if($nullJsVars == ''){
+                        $nullJsVars .= "var _$$key";
+                    }
+                    else{
+                        $nullJsVars .= ",_$$key";
+                    }
+                }
+                else{
+                    $realValue = null;
+                    if(is_string($value)){
+                        $realValue = "'$value'";
+                    }
+                    else if(is_array($value)){
+                        $realValue = json_encode($value);
+                    }
+                    else{
+                        $realValue = $value;
+                    }
+                    if($notNullJsVars == ''){
+                        $notNullJsVars .= "var _$$key = $realValue";
+                    }
+                    else{
+                        $notNullJsVars .= ",_$$key = $realValue";
+                    }
+                }
+            }
+            $nullJsVars .= ';';
+            $notNullJsVars .= ';';
+            $this->addFrontJsBlock($nullJsVars.$notNullJsVars);
+        }
+
+        if(!empty($this->jsBlocks)){
+            $pattern = '/(<\s*\/body\s*>)()/i';
+            $blocks = '<script src="https://unpkg.com/axios/dist/axios.min.js"></script>';
+
+            foreach($this->jsBlocks as $jsBlock){
+                $blocks .= "<script>$jsBlock</script>";
+            }
+
+            $page = preg_replace($pattern, '\1' . $blocks . '\2', $page);
+        }
+
+        return $page;
+    }
+
+    private function handleOptions($options)
     {
         $initOptions = explode('-', $options);
         $realOptions = [];
@@ -191,8 +318,8 @@ class LiTem
         $realFileName = null;
 
         foreach ($allowExtList as $ae) {
-            if(strpos($ae, '.') === false)
-                $tmpFileName = $filePath . '.' .$ae;
+            if (strpos($ae, '.') === false)
+                $tmpFileName = $filePath . '.' . $ae;
             else
                 $tmpFileName = $filePath . $ae;
             if (file_exists($tmpFileName)) {
@@ -242,12 +369,25 @@ class LiTem
 
     public function addReplacement($key, $value)
     {
-        $this->replacements[$key] = $value;
+        $this->config['replacements'][$key] = $value;
     }
 
     public function addReplacements($array)
     {
-        $this->replacements += $array;
+        $this->config['replacements'] += $array;
+    }
+
+    public function addJsVar($varName, $value = null)
+    {
+        $this->jsVars[$varName] = $value;
+    }
+
+    public function addJsBlock($block){
+        $this->jsBlocks[] = $block;
+    }
+
+    public function addFrontJsBlock($block){
+        array_unshift($this->jsBlocks, $block);
     }
 
     /**
@@ -263,14 +403,14 @@ class LiTem
         return $this->config[$name];
     }
 
-    function __set($name, $value)
-    {
-        // TODO: Implement __set() method.
-        if(isset($this->$name)){
-            $this->$name = $value;
-        }
-        else{
-            $this->config[$name] = $value;
-        }
-    }
+//    function __set($name, $value)
+//    {
+//        // TODO: Implement __set() method.
+//        if(isset($this->$name)){
+//            $this->$name = $value;
+//        }
+//        else{
+//            $this->config[$name] = $value;
+//        }
+//    }
 }
